4e766ffc041fb5b926bc7d0c590d3644c0d3e451
freelancersazal <freelancersazal@gmail.com>
2024-02-07 20:10:30 +0600

    home 3 added

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,6 +197,44 @@ $(function () {
     });
 
 
+    //======listing 3 slider======
+    $('.listing_3_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        dots: false,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
 
     //======testimonial slider======
     $('.testimonial_slider').slick({
